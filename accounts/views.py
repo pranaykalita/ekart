0d876dabf8315cf52a6464bae2d9d57c28f1a39c
pranaykalita <pranaykalita2@gmail.com>
2023-02-13 17:24:20 +0530

    account page loginrequired & redirect if loggedin

--- accounts/views.py
+++ accounts/views.py
@@ -6,47 +6,45 @@ from django.contrib.auth.models import User,auth
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def logout(request):
-    auth.logout(request)
-    return render(request,'mainapp/home.html')
-
-def login(request):
-    if User.is_authenticated :
-        print("if part loggedin")
-        print(User.is_authenticated)
-    else:
-        print("else not login")
-        print(User.is_authenticated)
-        if request.method == "POST":
-            username = request.POST.get("username")
-            password = request.POST.get("password")
-            user = auth.authenticate(username=username, password=password)
-            if user is not None:
-                auth.login(request, user)
-                return render(request, 'account/account.html')
-            else:
-                return redirect('/')
-    return render(request, 'account/login.html')
-
 
 def register(request):
-
-    # get registration form post requests
-
     if request.method == "POST":
-
         username = request.POST.get("Username")
         firstname = request.POST.get("FirstName")
         lastname = request.POST.get("LastName")
         email = request.POST.get("email")
         passwordcnf = request.POST.get("cnfpassword")
 
-        # save to database
-        user = User.objects.create_user(username=username,password=passwordcnf,email=email,first_name=firstname,last_name=lastname)
+        user = User.objects.create_user(username=username,
+                                        first_name=firstname,
+                                        last_name=lastname,
+                                        email=email,
+                                        password=passwordcnf)
         user.save
-        return redirect('account/login.html')
-
+        return render(request, 'account/login.html')
     return render(request, 'account/register.html')
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('/account')
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'account/account.html')
+        else:
+            return redirect('/')
+    else:
+        return render( request, 'account/login.html')
+
+   
+
+def logout(request):
+    auth.logout(request)
+    return render(request,'mainapp/home.html')
+
 
 # add decorator
 @login_required(login_url='account/login')
